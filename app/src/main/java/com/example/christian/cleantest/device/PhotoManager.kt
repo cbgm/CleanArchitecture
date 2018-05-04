fec503e8f417100e283169c127ae201fc3508270
Christian B <christian.bergmann@kunstadt.de>
2018-05-04 10:04:20 +0200

    Init Manager class

--- app/src/main/java/com/example/christian/cleantest/device/PhotoManager.kt
+++ app/src/main/java/com/example/christian/cleantest/device/PhotoManager.kt
@@ -1,4 +1,22 @@
 package com.example.christian.cleantest.device
 
-class PhotoManager {
+import android.content.Context
+import javax.inject.Inject
+
+class PhotoManager @Inject constructor(val context: Context) {
+
+    companion object {
+        //const für callbacks
+    }
+
+    fun initPicking(){
+
+    }
+
+
+    //adapter
+
+
+
+
 }
