b79cd2c69d5751f5b8969b2a45d02accb044986f
Max <held-maximilian@t-online.de>
2018-05-07 11:54:00 +0200

    Update renamed SaveUtil to ImageUtil and implemented loadImage method; implemented delete Method in SharedPreferencesUtil

--- app/src/main/java/com/example/christian/cleantest/device/photo/SharedPreferencesUtil.kt
+++ app/src/main/java/com/example/christian/cleantest/device/photo/SharedPreferencesUtil.kt
@@ -1,19 +1,26 @@
 package com.example.christian.cleantest.device.photo
 
 import android.content.Context
+import android.content.SharedPreferences
 
 class SharedPreferencesUtil {
     companion object {
         private const val SHAREDPREFERENCE = "Files"
 
         fun get(id: String, context: Context): String {
-            val sharedPreferences = context.getSharedPreferences(SHAREDPREFERENCE, Context.MODE_PRIVATE)
-            return sharedPreferences.getString(id, null)
+            return getSharedPreferences(context).getString(id, null)
         }
 
         fun set(id: String, context: Context) {
-            val sharedPreferences = context.getSharedPreferences(SHAREDPREFERENCE, Context.MODE_PRIVATE)
-            sharedPreferences.edit().putString(id, id)
+            getSharedPreferences(context).edit().putString(id, id).apply()
+        }
+
+        fun delete(id: String, context: Context) {
+            getSharedPreferences(context).edit().remove(id).apply()
+        }
+
+        private fun getSharedPreferences(context: Context): SharedPreferences {
+            return context.getSharedPreferences(SHAREDPREFERENCE, Context.MODE_PRIVATE)
         }
     }
 }
