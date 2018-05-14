2fadd14336bd978a9a1c09ec3750a7894fd586cb
Christian B <christian.bergmann@kunstadt.de>
2018-05-14 08:29:16 +0200

    Update load and save image

--- app/src/main/java/com/example/christian/cleantest/device/photo/PhotoManager.kt
+++ app/src/main/java/com/example/christian/cleantest/device/photo/PhotoManager.kt
@@ -9,6 +9,7 @@ import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Build
+import android.os.Environment
 import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
