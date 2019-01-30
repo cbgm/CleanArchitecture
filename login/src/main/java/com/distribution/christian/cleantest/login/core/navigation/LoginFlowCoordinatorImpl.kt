d52e886728499ec8b2746f2dbb0127ba5eb380f9
Christian B <christian.bergmann@kunstadt.de>
2019-01-30 13:47:06 +0100

    initial login  commit

---
+++ login/src/main/java/com/distribution/christian/cleantest/login/core/navigation/LoginFlowCoordinatorImpl.kt
@@ -0,0 +1,28 @@
+package com.distribution.christian.cleantest.login.core.navigation
+
+import com.distribution.christian.cleantest.core.core.navigation.BaseCoordinatorImpl
+import com.distribution.christian.cleantest.core.core.navigation.deeplink.DeepLinkIdentifier
+import com.distribution.christian.cleantest.core.core.util.extension.navigateToEvents
+import com.distribution.christian.cleantest.core.core.util.extension.navigateToShop
+
+class LoginFlowCoordinatorImpl : BaseCoordinatorImpl() {
+
+   override fun navigateDeepLink() {
+      deepLinkHandler.getDeepLink()
+            ?.let {
+               when (it.action) {
+                  DeepLinkIdentifier.EVENTS-> activity.navigateToEvents(activity)
+                  DeepLinkIdentifier.SHOP -> activity.navigateToShop(activity)
+                  else -> activity.navigateToEvents(activity)
+               }
+            }
+   }
+
+   override fun navigateLink() {
+      activity.navigateToEvents(activity)
+   }
+
+   override fun onDeepLinkBack() {
+      //not needed
+   }
+}
