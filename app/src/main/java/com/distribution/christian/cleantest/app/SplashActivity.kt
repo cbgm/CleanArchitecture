3d538cbde35f58170e7ee93beedeab0bf3ceea73
Christian B <christian.bergmann@kunstadt.de>
2019-04-04 15:05:23 +0200

    Update bookmarking

--- app/src/main/java/com/distribution/christian/cleantest/app/SplashActivity.kt
+++ app/src/main/java/com/distribution/christian/cleantest/app/SplashActivity.kt
@@ -1,14 +1,18 @@
 package com.distribution.christian.cleantest.app
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ValueAnimator
 import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.ViewPropertyAnimatorCompat
 import com.distribution.christian.cleantest.R
 import com.distribution.christian.cleantest.app.core.navigation.RootFlowCoordinatorImpl
+import com.distribution.christian.cleantest.core.core.util.listener.AnimationPropertyEndListener
 import org.koin.android.ext.android.inject
 import timber.log.Timber
 
@@ -17,17 +21,19 @@ class SplashActivity : AppCompatActivity() {
 
    private val rootFlowCoordinatorImpl: RootFlowCoordinatorImpl by inject()
 
-   private val title: TextView by lazy { findViewById<TextView>(R.id.title_text) }
+   //private val title: TextView by lazy { findViewById<TextView>(R.id.title_text) }
+   private val title: TextView by lazy { findViewById<TextView>(R.id.title) }
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(R.layout.activity_splash2)
       startAnimation()
+      animate()
       Timber.v("release - debug switch test")
    }
 
    private fun startAnimation() {
-      val startSize = 12f // Size in pixels
+      /*val startSize = 12f // Size in pixels
       val endSize = 38f
       val animationDuration: Long = 600
       val animator = ValueAnimator.ofFloat(startSize, endSize)
@@ -44,7 +50,45 @@ class SplashActivity : AppCompatActivity() {
             startRouting()
          }
       })
-      animator.start()
+      animator.start()*/
+
+   }
+
+   private fun animate() {
+      val logoImageView = findViewById<View>(R.id.img_logo) as ImageView
+      val container = findViewById<View>(R.id.container) as ViewGroup
+
+      ViewCompat.animate(logoImageView)
+            .translationY(-250f)
+            .setStartDelay(300)
+            .setDuration(1000)
+            .setInterpolator(
+                  DecelerateInterpolator(1.2f)
+            )
+            .start()
+      var viewAnimator: ViewPropertyAnimatorCompat
+
+      for (i in 0 until container.childCount) {
+         val v = container.getChildAt(i)
+
+
+         viewAnimator = ViewCompat.animate(v)
+               .translationY(10F)
+               .alpha(1F)
+               .setStartDelay(((300 * i) + 500).toLong())
+               .setDuration(1000)
+
+         viewAnimator.setInterpolator(DecelerateInterpolator())
+               .start()
+
+         if (i == container.childCount - 1) {
+            viewAnimator.setListener(object : AnimationPropertyEndListener() {
+               override fun onAnimationEnd(view: View?) {
+                  startRouting()
+               }
+            })
+         }
+      }
    }
 
    private fun startRouting() {
---
+++ core/src/main/java/com/distribution/christian/cleantest/core/core/util/listener/AnimationPropertyEndListener.kt
@@ -0,0 +1,20 @@
+package com.distribution.christian.cleantest.core.core.util.listener
+
+import android.view.View
+import androidx.core.view.ViewPropertyAnimatorListener
+
+
+open class AnimationPropertyEndListener : ViewPropertyAnimatorListener {
+   override fun onAnimationEnd(view: View?) {
+      TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+   }
+
+   override fun onAnimationCancel(view: View?) {
+      //not used
+   }
+
+   override fun onAnimationStart(view: View?) {
+      //not used
+   }
+
+}
